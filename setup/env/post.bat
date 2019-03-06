403249091b3611796803a879447d9f0b4e1cc3ce
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-03-06 19:00:08 +0700

    bugfix

--- setup/env/post.bat
+++ setup/env/post.bat
@@ -3,10 +3,11 @@
 %run% conda config --env --add channels conda-forge
 %run% conda config --env --add channels defaults
 
+%run% conda install ".\enaml-video-app-0.1.0-py_0.tar.bz2"
 %run% conda remove --force --yes pyqtgraph
-%run% pip install git+https://github.com/pyqtgraph/pyqtgraph.git
-%run% conda remove --force --yes qt pyqt sip
-%run% pip install pyside2
+%run% conda install --copy --yes pyqtgraph
+%run% pip install --ignore-installed git+https://github.com/pyqtgraph/pyqtgraph.git
+%run% pip install --ignore-installed pyside2
 
 :: %run% python -m ipykernel install --user --name %env%
 
